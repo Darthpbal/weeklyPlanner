588fea9ac7b01d7c9fb3fc7acb0f09fd47ce8d03
darthpbal <ronquillo.paul@gmail.com>
2016-10-25 02:37:09 -0700

    rearranged menu stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cstdlib>
 #include "Week.hpp"
 using namespace std;
 
@@ -7,17 +8,35 @@ int main(){
     cout << "Hello World\n";
 
     Week myWeek;
-    string option, day, tim;
+    string option, task, day, timeStart, timeEnd;
     while(option != "exit"){
       myWeek.printWeek();
       cout << "what should I do?\n";
       cin >> option;
-      cout << "what day??\n";
-      cin >> day;
-      cout << "what time?\n";
-      cin >> tim;
-      myWeek.setWeekBlock(option, day, tim);
+      if(option == "exit") exit(0);
+
+      cout << "What is the task to do?\n";
+      cin >> task;
+
       if(option == "clear") myWeek.emptyWeek();
+
+      else if(option == "setblock"){
+        cout << "what day?\n";
+        cin >> day;
+        cout << "what time?\n";
+        cin >> timeStart;
+        myWeek.setWeekBlock(task, day, timeStart);
+      }
+
+      else if(option == "setrange"){
+        cout << "what day?\n";
+        cin >> day;
+        cout << "start time?\n";
+        cin >> timeStart;
+        cout << "end time?\n";
+        cin >> timeEnd;
+        myWeek.setWeekBlockRange(task, day, timeStart, timeEnd);
+      }
     }
 
   return 0;
