f7916ebd9fd04022af1e9a6df1bd19222e3a7e84
darthpbal <ronquillo.paul@gmail.com>
2016-10-26 15:38:31 -0700

    added to my todo list

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
 /*
 todo
-add textfile based load functions
-add html reporting
-add fancy style shiz to the html report
+add Textfile based load functions (may be relatively harder feature than others if a week has significant number of empty cells)
+add ability for the week frame size to scale based on screen size. Currently only sized for my GIR laptop. (this may mean that there might have to be a new scheme for storing/displaying the weekblocks. Could get complex!)
+add A function for counting how many cells are a certain task in a day for returning number of hours spent on the task that day. This can be then expanded for every day of the week in total.
+If successful with function conting elements, maybe create a part of the display showing whether or not the time for this task has met some given threshold. Like 3 hrs homework for each hour of lecture. you ask the system how many lecture hours, it returns a float, and then the user sets a threshold for a specific label which is updated every time an action is done on the week. This could get complex, but I feel like if I'm careful, I can conserve a lot of code. I think I'd need to create member functions specifically for cleanly prompting users for inputs like day and time and then create a better tolerance for typos and simplify the controls...... sounds like fun.
+add Html reporting? Do I care? add fancy style shiz like bootstrap to the html report?
 
-use the ostream class type as a function argument to save typing on the save file
-function and just pass cout or an fstream to the print functions. (remember to pas by refernce)
+Use the ostream class type as a function argument to save typing on the save file function and just pass cout or an ofstream to the print functions. (remember to pass by refernce, no need to create uneccesary memory stuffs)
+This would essencially allow me to abstract out the "cout" part of any functions I might also want the same behavior with a file. This works because the "<<" and ">>" operators somehow belong to a root "stream" class that is used within many other classes like fstream (for text file manipulations),  streamstream (for manipulating strings like a stream), and iostream (like cout and cin).
+In this way, both iostream and fstream are said to "inherit" the properties of a root class called ostream.
+Aside from ostream being a more "root" parent layer of iostream and ofstream, I do not understand it's functional differences from iostream aside from maybe having different member functions
+This should result in much less code being used for outputting text and file writing combined. This advantage is made because the file writingg is being done exactly the same way as the main display, so I'm repeating a ton of code to just write to a text file. So this should reduce that related code by half.
 */
+
+
+
 #include <iostream>
 #include <string>
 #include <cstdlib>
